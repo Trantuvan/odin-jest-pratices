1160b860cfc430fa541859e6fda1ebd27e02befe
trantuvan <trantuvan.kan@gmail.com>
2022-11-07 21:07:41 +0700

    remove console.log

--- src/capitalize.js
+++ src/capitalize.js
@@ -6,6 +6,5 @@ export default function capitalize(str) {
   const strArrayWith1stLetterUppercase = strArrayWithWhiteSpaceAsSeparators.map(
     (str) => str.charAt(0).toUpperCase() + str.slice(1)
   );
-  console.log(strArrayWith1stLetterUppercase.join(" "));
   return strArrayWith1stLetterUppercase.join(" ");
 }
